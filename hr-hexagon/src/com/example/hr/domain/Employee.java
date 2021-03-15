d721343561021766861ce9f610b99e99891b9462
hakan.ozerden <hakanozerden@yahoo.com>
2021-03-15 21:33:59 +0300

    Initial commit.

---
+++ hr-hexagon/src/com/example/hr/domain/Employee.java
@@ -0,0 +1,24 @@
+package com.example.hr.domain;
+
+/**
+ * @author hakan.ozerden
+ */
+public class Employee {
+
+	private Identity identity;
+
+	private FullName fullname;
+
+	private Money salary;
+
+	private Iban iban;
+
+	private BirthYear birthYear;
+
+	private Department department;
+
+	private JobType jobType;
+
+	private Photo photo;
+
+}
